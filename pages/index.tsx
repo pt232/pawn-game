72c9744dcb44517c39e0d48430597b7dc40d82bf
Tobias Peters <peterstobias2302@gmail.com>
2023-02-25 11:40:38 +0100

    initial: add basic nextjs project with tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Pawn Game</title>
+        <meta name="description" content="A simple chess game" />
+      </Head>
+      <main>
+        <h1 className="text-3xl font-bold">Hello World</h1>
+      </main>
+    </>
+  );
+}
