112eb3bf39236cd1b15349f7213730f202d1f12d
Tobias Peters <peterstobias2302@gmail.com>
2023-03-07 18:10:20 +0100

    feat(clock): add clock component to the board

--- components/board.tsx
+++ components/board.tsx
@@ -1,79 +1,90 @@
+import Clock from "./clock";
 import Tile from "./tile";
 
 export default function Board() {
   return (
-    <div className="grid grid-cols-8 overflow-hidden rounded-[4px] bg-red-50">
-      <Tile color="white" number="8" finishPosition="top" />
-      <Tile color="black" finishPosition="top" />
-      <Tile color="white" finishPosition="top" />
-      <Tile color="black" finishPosition="top" />
-      <Tile color="white" finishPosition="top" />
-      <Tile color="black" finishPosition="top" />
-      <Tile color="white" finishPosition="top" />
-      <Tile color="black" finishPosition="top" />
+    <>
+      <div className="flex justify-end">
+        <Clock color="black" />
+      </div>
 
-      <Tile color="black" pieceColor="black" number="7" />
-      <Tile color="white" pieceColor="black" />
-      <Tile color="black" pieceColor="black" />
-      <Tile color="white" pieceColor="black" />
-      <Tile color="black" pieceColor="black" />
-      <Tile color="white" pieceColor="black" />
-      <Tile color="black" pieceColor="black" />
-      <Tile color="white" pieceColor="black" />
+      <div className="my-2 grid grid-cols-8 overflow-hidden rounded-[4px] bg-red-50">
+        <Tile color="white" number="8" finishPosition="top" />
+        <Tile color="black" finishPosition="top" />
+        <Tile color="white" finishPosition="top" />
+        <Tile color="black" finishPosition="top" />
+        <Tile color="white" finishPosition="top" />
+        <Tile color="black" finishPosition="top" />
+        <Tile color="white" finishPosition="top" />
+        <Tile color="black" finishPosition="top" />
 
-      <Tile color="white" number="6" />
-      <Tile color="black" />
-      <Tile color="white" />
-      <Tile color="black" />
-      <Tile color="white" />
-      <Tile color="black" />
-      <Tile color="white" />
-      <Tile color="black" />
+        <Tile color="black" pieceColor="black" number="7" />
+        <Tile color="white" pieceColor="black" />
+        <Tile color="black" pieceColor="black" />
+        <Tile color="white" pieceColor="black" />
+        <Tile color="black" pieceColor="black" />
+        <Tile color="white" pieceColor="black" />
+        <Tile color="black" pieceColor="black" />
+        <Tile color="white" pieceColor="black" />
 
-      <Tile color="black" number="5" />
-      <Tile color="white" />
-      <Tile color="black" />
-      <Tile color="white" />
-      <Tile color="black" />
-      <Tile color="white" />
-      <Tile color="black" />
-      <Tile color="white" />
+        <Tile color="white" number="6" />
+        <Tile color="black" />
+        <Tile color="white" />
+        <Tile color="black" />
+        <Tile color="white" />
+        <Tile color="black" />
+        <Tile color="white" />
+        <Tile color="black" />
 
-      <Tile color="white" number="4" />
-      <Tile color="black" />
-      <Tile color="white" />
-      <Tile color="black" />
-      <Tile color="white" />
-      <Tile color="black" />
-      <Tile color="white" />
-      <Tile color="black" />
+        <Tile color="black" number="5" />
+        <Tile color="white" />
+        <Tile color="black" />
+        <Tile color="white" />
+        <Tile color="black" />
+        <Tile color="white" />
+        <Tile color="black" />
+        <Tile color="white" />
 
-      <Tile color="black" number="3" />
-      <Tile color="white" />
-      <Tile color="black" />
-      <Tile color="white" />
-      <Tile color="black" />
-      <Tile color="white" />
-      <Tile color="black" />
-      <Tile color="white" />
+        <Tile color="white" number="4" />
+        <Tile color="black" />
+        <Tile color="white" />
+        <Tile color="black" />
+        <Tile color="white" />
+        <Tile color="black" />
+        <Tile color="white" />
+        <Tile color="black" />
 
-      <Tile color="white" pieceColor="white" number="2" />
-      <Tile color="black" pieceColor="white" />
-      <Tile color="white" pieceColor="white" />
-      <Tile color="black" pieceColor="white" />
-      <Tile color="white" pieceColor="white" />
-      <Tile color="black" pieceColor="white" />
-      <Tile color="white" pieceColor="white" />
-      <Tile color="black" pieceColor="white" />
+        <Tile color="black" number="3" />
+        <Tile color="white" />
+        <Tile color="black" />
+        <Tile color="white" />
+        <Tile color="black" />
+        <Tile color="white" />
+        <Tile color="black" />
+        <Tile color="white" />
 
-      <Tile color="black" number="1" letter="a" finishPosition="bottom" />
-      <Tile color="white" letter="b" finishPosition="bottom" />
-      <Tile color="black" letter="c" finishPosition="bottom" />
-      <Tile color="white" letter="d" finishPosition="bottom" />
-      <Tile color="black" letter="e" finishPosition="bottom" />
-      <Tile color="white" letter="f" finishPosition="bottom" />
-      <Tile color="black" letter="g" finishPosition="bottom" />
-      <Tile color="white" letter="h" finishPosition="bottom" />
-    </div>
+        <Tile color="white" pieceColor="white" number="2" />
+        <Tile color="black" pieceColor="white" />
+        <Tile color="white" pieceColor="white" />
+        <Tile color="black" pieceColor="white" />
+        <Tile color="white" pieceColor="white" />
+        <Tile color="black" pieceColor="white" />
+        <Tile color="white" pieceColor="white" />
+        <Tile color="black" pieceColor="white" />
+
+        <Tile color="black" number="1" letter="a" finishPosition="bottom" />
+        <Tile color="white" letter="b" finishPosition="bottom" />
+        <Tile color="black" letter="c" finishPosition="bottom" />
+        <Tile color="white" letter="d" finishPosition="bottom" />
+        <Tile color="black" letter="e" finishPosition="bottom" />
+        <Tile color="white" letter="f" finishPosition="bottom" />
+        <Tile color="black" letter="g" finishPosition="bottom" />
+        <Tile color="white" letter="h" finishPosition="bottom" />
+      </div>
+
+      <div className="flex justify-end">
+        <Clock color="white" />
+      </div>
+    </>
   );
 }
---
+++ components/clock.tsx
@@ -0,0 +1,20 @@
+import { Clock12 } from "lucide-react";
+
+type ClockProps = {
+  color: "white" | "black";
+};
+
+export default function Clock({ color }: ClockProps) {
+  return (
+    <div
+      className={`flex w-max items-center gap-2 rounded-md py-1 px-3 text-lg font-semibold ${
+        color === "white"
+          ? "bg-secondary-300 text-secondary-900"
+          : "bg-secondary-900 text-secondary-300"
+      }`}
+    >
+      <Clock12 size={18} />
+      05:00
+    </div>
+  );
+}
