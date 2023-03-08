a36bdea874e7198e4695ed2fa6bbf487e357cea4
Tobias Peters <peterstobias2302@gmail.com>
2023-03-08 17:58:30 +0100

    feat(about-dialog): add a seperate about dialog

---
+++ components/dialog/AboutDialog.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Dialog from "../ui/dialog";
+
+type AboutDialogProps = {
+  isOpen: boolean;
+  close: () => void;
+};
+
+export default function AboutDialog({ isOpen, close }: AboutDialogProps) {
+  return (
+    <Dialog isOpen={isOpen} title="About" close={close}>
+      <p className="mb-3">
+        The Pawn Game is a simple and fun multiplayer web app that I created as
+        a personal project to practice my web development skills. Have fun
+        playing!
+      </p>
+      <p>
+        The game&apos;s design is heavily inspired by{" "}
+        <Link
+          href="https://www.chess.com/"
+          target="_blank"
+          className="text-link-500 hover:underline"
+        >
+          Chess.com
+        </Link>
+        .
+      </p>
+    </Dialog>
+  );
+}
--- components/page-header.tsx
+++ components/page-header.tsx
@@ -2,63 +2,43 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { ExternalLink } from "lucide-react";
-import Dialog from "./ui/dialog";
+import AboutDialog from "./dialog/AboutDialog";
 
 export default function PageHeader() {
   const [isAboutDialogOpen, setIsAboutDialogOpen] = useState(false);
 
   return (
-    <>
-      <header className="mb-4 flex items-center justify-between rounded-md py-4 text-secondary-300">
-        <Link href="/">
-          <Image src="/logo.svg" width={128} height={24} alt="Logo" />
-        </Link>
-        <nav>
-          <ul className="flex gap-x-4">
-            <li>
-              <button
-                className="transition-colors hover:text-white"
-                onClick={() => setIsAboutDialogOpen(true)}
-              >
-                About
-              </button>
-            </li>
-            <li>
-              <Link
-                href="https://github.com/pt232/pawn-game"
-                target="_blank"
-                className="flex items-center gap-1 transition-colors hover:text-white"
-              >
-                GitHub
-                <ExternalLink size={16} />
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+    <header className="mb-4 flex items-center justify-between rounded-md py-4 text-secondary-300">
+      <Link href="/">
+        <Image src="/logo.svg" width={128} height={24} alt="Logo" />
+      </Link>
+      <nav>
+        <ul className="flex gap-x-4">
+          <li>
+            <button
+              className="transition-colors hover:text-white"
+              onClick={() => setIsAboutDialogOpen(true)}
+            >
+              About
+            </button>
+          </li>
+          <li>
+            <Link
+              href="https://github.com/pt232/pawn-game"
+              target="_blank"
+              className="flex items-center gap-1 transition-colors hover:text-white"
+            >
+              GitHub
+              <ExternalLink size={16} />
+            </Link>
+          </li>
+        </ul>
+      </nav>
 
-      <Dialog
+      <AboutDialog
         isOpen={isAboutDialogOpen}
-        title="About"
         close={() => setIsAboutDialogOpen(false)}
-      >
-        <p className="mb-3">
-          The Pawn Game is a simple and fun multiplayer web app that I created
-          as a personal project to practice my web development skills. Have fun
-          playing!
-        </p>
-        <p>
-          The game&apos;s design is heavily inspired by{" "}
-          <Link
-            href="https://www.chess.com/"
-            target="_blank"
-            className="text-link-500 hover:underline"
-          >
-            Chess.com
-          </Link>
-          .
-        </p>
-      </Dialog>
-    </>
+      />
+    </header>
   );
 }
